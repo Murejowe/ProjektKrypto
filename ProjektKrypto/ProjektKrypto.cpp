2fde361bc851a004aef9f47ea73a788d918714b4
szymon <szymon@MUREJOWYKOMP>
2025-12-01 00:24:57 +0100

    bity w vectorach

--- ProjektKrypto/ProjektKrypto.cpp
+++ ProjektKrypto/ProjektKrypto.cpp
@@ -1,20 +1,39 @@
-﻿// ProjektKrypto.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
+﻿#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 #include <iostream>
+#include <windows.h>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+    int width, height, channels;
+    unsigned char* data = stbi_load("Example1.png", &width, &height, &channels, 3);
+    std::vector<std::vector<int>> bity;
+    if (!data) {
+        std::cout << "Błąd wczytywania obrazu!\n";
+        return -1;
+    }
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+
+            // Obliczamy indeks bazowy piksela
+            int index = (y * width + x) * channels;
+            std::vector<int> bit_kanal;
+            bit_kanal.push_back(data[index]);
+            bit_kanal.push_back(data[index+1]);
+            bit_kanal.push_back(data[index+2]);
+            bity.push_back(bit_kanal);
+        }
+    }
+    for (auto& wektor : bity) {
+        for (auto& piksel : wektor) {
+            std::cout << piksel << " ";
+        }
+        std::cout << "\n";
+    }
+    stbi_image_free(data);
+    return 0;
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
-
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
